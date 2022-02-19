35486a82bd4a270ad666dfab74b898f3962d7661
Jefferson Dias <jefer.ld@hotmail.com>
2022-02-18 21:47:15 -0300

    saveProject function from editProjectList creating

--- src/edits/editProjectList.js
+++ src/edits/editProjectList.js
@@ -6,6 +6,7 @@ import { useNavigate, useParams } from "react-router-dom"
 export default function EditProjectList(){
     let {id} = useParams()
     const [project, setProject] = useState({name:"", numberSprint:"", price:""})
+    const navigate = useNavigate()
 
     useEffect(()=>{
         axios.get(`http://localhost:3001/projects/${id}/getItem`)
@@ -20,15 +21,24 @@ export default function EditProjectList(){
         })
     }
 
+    const saveProject = (event)=>{
+        axios.put(`http://localhost:3001/projects/${id}/changeAll`,project)
+            .then(()=>{
+                navigate('/project-list')
+            })
+
+        event.preventDefault()
+    }
+
     return(
         <div className="d-flex form-style">
-            <form className="mx-auto">
+            <form onSubmit={saveProject} className="mx-auto">
                 {JSON.stringify(project)}
                 <h3 className="text-center">Edição de projetos</h3>
                 <input onChange={changeProject} className="form-control py-1" type="text" name="name" value={project.name} />
                 <input onChange={changeProject} className="form-control py-1" type="number" name="numberSprint" value={project.numberSprint} />
                 <input onChange={changeProject} className="form-control py-1" type="text" name="price" value={project.price} />
-                <button className="btn btn-primary py-2 w-100">Salvar</button>
+                <button type="submit" className="btn btn-primary py-2 w-100">Salvar</button>
             </form>
         </div>
     )
